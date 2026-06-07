fb2453fa4d0bff5ab0f9cfa3d3f09c869ac36edb
sirsona <swaleh489@gmail.com>
2026-06-07 14:37:52 +0300

    chore: remove commented code

--- payments-backend/index.js
+++ payments-backend/index.js
@@ -1,24 +1,3 @@
-// require("dotenv").config();
-
-// const cors = require("cors");
-
-// const express = require("express");
-// const app = express();
-
-// app.use(cors());
-// app.use(express.json());
-
-
-// app.get("/health", (req, res) => {
-//     res.status(200).json({ status: "ok", timestamp: new Date().toISOString() });
-// });
-
-
-// const PORT = process.env.PORT || 3001;
-// app.listen(PORT, () => {
-//     console.log(`Server on : ${PORT}`);
-// });
-
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
